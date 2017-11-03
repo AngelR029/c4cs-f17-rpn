772407a6156cb515a2823500288746588690bf0e
angel <angelrod@umich.edu>
2017-11-02 20:56:19 -0700

    added color functionality, fixed log functionality

--- rpn.py
+++ rpn.py
@@ -2,6 +2,8 @@
 
 import operator
 import readline
+from termcolor import colored, cprint
+import colorama
 
 operators = {
     '+': operator.add,
@@ -13,6 +15,7 @@ operators = {
 
 mode = {
     "log": False,        
+    "color": True,
 }
 
 def calculate(myarg):
@@ -30,19 +33,40 @@ def calculate(myarg):
             except ValueError:
                 function = operators[token]
                 arg2 = stack.pop()
+                
+                if len(stack) == 0:
+                    raise TypeError("Too many operators")
                 arg1 = stack.pop()
                 result = function(arg1, arg2)
                 stack.append(result)
-        if mode["log"]:
-            print(stack)
-    if len(stack) != 1:
-        raise TypeError("Too many parameters")
+        if(len(stack) > 0): 
+            if mode["log"]:
+                if mode["color"]:
+                    for number in stack:
+                        if number < 0:
+                            print('['+ colored(number, "red") + ']', end='')
+                        else :
+                            print('[' + colored(number, 'green') + ']', end='')
+                    print()
+                else:
+                    print(stack)
+    if len(stack) == 0:
+        return None
     return stack.pop()
 
 def main():
     while True:
-        result = calculate(input("rpn calc> "))
-        print("Result: ", result)
+        result = calculate(input("rpn calc> ")) 
+        if result == None:
+            pass
+        else:
+            if mode["color"]: 
+                if result < 0:
+                    print("result> " + colored(result, "red"))
+                else :
+                    print("result> " + colored(result, 'green'))
+            else:
+                print("result> " + result)
 
 if __name__ == '__main__':
     main()
